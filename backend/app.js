98a646470bdc161d00ddcd83be700c6b25594cdf
Alexandr kudryavtsev <santyagobatkich@yandex.ru>
2022-05-10 22:00:06 +0700

    Cors test

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 require('dotenv').config();
 
+const cors = require('cors');
+
 const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
@@ -26,12 +28,12 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-const allowedCors = [
-  'https://santyagobatkich.students.nomoredomains.xyz',
-  'http://santyagobatkich.students.nomoredomains.xyz',
-  'http://localhost:3000',
-  'https://localhost:3000',
-];
+// const allowedCors = [
+//   'https://santyagobatkich.students.nomoredomains.xyz',
+//   'http://santyagobatkich.students.nomoredomains.xyz',
+//   'http://localhost:3000',
+//   'https://localhost:3000',
+// ];
 
 const {
   userCreate, login,
@@ -40,26 +42,28 @@ const {
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 // eslint-disable-next-line consistent-return
-app.use((req, res, next) => {
-  const { origin } = req.headers;
+// app.use((req, res, next) => {
+//   const { origin } = req.headers;
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//     res.header('Access-Control-Allow-Credentials', true);
+//   }
 
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
+//   const { method } = req;
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     return res.end();
+//   }
 
-  next();
-});
+//   next();
+// });
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
