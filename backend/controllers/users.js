85d95e17eb2c8a52b71a8122789ffb8e2aae96fe
Alexandr kudryavtsev <santyagobatkich@yandex.ru>
2022-05-11 18:30:42 +0700

    test secure

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -135,7 +135,9 @@ const login = (req, res, next) => {
         NODE_ENV === 'production' ? JWT_SECRET : 'topsecret-token',
         { expiresIn: '7d' },
       );
-      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true, SameSite: 'None' });
+      res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7, httpOnly: true, SameSite: 'None', secure: true,
+      });
       res.send({ token });
     })
     .catch(() => {
