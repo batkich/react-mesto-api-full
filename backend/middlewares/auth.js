b6651c39016d7563062dd97699ceeb175ed9b9d0
Alexandr kudryavtsev <santyagobatkich@yandex.ru>
2022-05-11 22:46:01 +0700

    test domain

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,14 +2,12 @@ const jwt = require('jsonwebtoken');
 
 const Unauthorized = require('../errors/unauthorized');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
-
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'topsecret-token');
+    payload = jwt.verify(token, 'topsecret-token');
   } catch (err) {
     next(new Unauthorized('Необходима авторизация'));
   }
